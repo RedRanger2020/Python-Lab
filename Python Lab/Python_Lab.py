2ef3f86249f93798dee39c36e457743e4d4bd099
red2016ranger <red2016ranger@gmail.com>
2024-09-24 12:30:25 +0300

    Создана конфигурация проекта

--- Python Lab/Python_Lab.py
+++ Python Lab/Python_Lab.py
@@ -1,1 +1,10 @@
 
+def main():
+    searchValues = ["cat","dog"]
+    countAmount = 1000
+    for value in searchValues:
+        print("Found")
+              
+
+if __name__ == '__main__':
+    main()
