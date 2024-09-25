fcc513fd407118b477f98631882a158567dd6b52
red2016ranger <red2016ranger@gmail.com>
2024-09-25 16:34:54 +0300

    Создана конфигурация проекта

--- Python Lab/env/Lib/site-packages/fp/errors.py
+++
@@ -1,7 +0,0 @@
-'''FreeProxy exceptions module'''
-
-class FreeProxyException(Exception):
-    '''Exception class with message as a required parameter'''
-    def __init__(self, message) -> None:
-        self.message = message
-        super().__init__(self.message)
